d8b019f71c9c52b947819b8bf0d878c9231fd85d
patil school sindhanur <noblelibrary56@gmail.com>
2025-04-29 15:04:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// Add any interactive JavaScript here
+// For example, smooth scrolling for navigation
+
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        const href = this.getAttribute('href');
+        if (href.startsWith('#')) {
+            e.preventDefault();
+            document.querySelector(href).scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
